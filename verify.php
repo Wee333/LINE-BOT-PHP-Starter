709f896eb245fe0e5b9414bba8795d67e0640395
Wee333 <taweesak287@gmail.com>
2017-07-12 01:05:01 +0700

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = '1YgQ/ztFkRih7fPRTGwfi7anbCiVttu8AfDKQBYeZhd1wfVeVsrnnL1Gt97WEaPiHEyQwNBhkX37MNVoyhrJxqFPILRHwSBh4sPTFoqA4dRZESP67CYHpm48jrzoMNz5IPFufDB/FOJnyToYRyZ8MAdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
